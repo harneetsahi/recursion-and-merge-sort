095281e0ef6689336b76f56b941f3a278d5ff81d
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-01 22:37:30 -0600

    function of iterative fib

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // iterative fibonacci
 
-function fib(arr) {
+function fib(n) {
     
     if (n == 0) return [];
     if (n == 1) return [0];
  
     let result = [0,1];
   
-
-    
+     for (let i = 2; i < n; i++) {
+        let nextVal = result[i-1] + result[i-2];
+        result.push(nextVal);    
+      }
+  
+    return result;
+  
 }
+
+console.log(fib(8)); // returns [0,1,1,2,3,5,8,13]
