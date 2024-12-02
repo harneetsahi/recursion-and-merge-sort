8d0ed57fded5ca8c0d8afc992e33599e174464fd
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-01 22:14:25 -0600

    Loop function

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function fib(arr) {
     if (arr.length == 1) return [0];
     if (arr.length == 2) return [0,1];
     
+    for (let i = arr.length; i< arr.length ; i++) {
+        
     
+    }
     
 }
