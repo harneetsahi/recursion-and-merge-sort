6fef2b7f9034a14fcaa5a210c4eb012cc66aa7fe
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-03 19:46:33 -0600

    formatted code

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function fib(n) {
  
     let result = [0,1];
   
-     for (let i = 2; i < n; i++) {
+    for (let i = 2; i < n; i++) {
         let nextVal = result[i-1] + result[i-2];
         result.push(nextVal);    
-      }
+    }
   
     return result;
   
@@ -22,16 +22,16 @@ console.log(fib(8)); // returns [0,1,1,2,3,5,8,13]
 
 function fibRec(n) {
   
-  if (n == 0) return [];
-  if (n == 1) return [0];
-  if (n == 2) return [0,1];
+    if (n == 0) return [];
+    if (n == 1) return [0];
+    if (n == 2) return [0,1];
   
-  let fib = fibRec(n-1);
+    let fib = fibRec(n-1);
   
-  let nextVal = fib[fib.length-1] + fib[fib.length-2];
+    let nextVal = fib[fib.length-1] + fib[fib.length-2];
     fib.push(nextVal);        
   
-  return fib;
+    return fib;
   
 }
 
@@ -41,29 +41,29 @@ console.log(fibRec(8));  // returns [0,1,1,2,3,5,8,13]
 
 function mergeSort(arr) {
   
-  if (arr.length <= 1) return arr;
+    if (arr.length <= 1) return arr;
 
-  let midpoint = Math.floor(arr.length / 2);
-  let leftArr = mergeSort(arr.slice(0, midpoint));
-  let rightArr = mergeSort(arr.slice(midpoint));
+    let midpoint = Math.floor(arr.length / 2);
+    let leftArr = mergeSort(arr.slice(0, midpoint));
+    let rightArr = mergeSort(arr.slice(midpoint));
   
-  return merge(leftArr, rightArr);
+    return merge(leftArr, rightArr);
 
 }
 
 function merge(leftArr, rightArr) {
   
-  let sortedArr = []
+    let sortedArr = []
   
-  while (leftArr.length && rightArr.length) {
-    if (leftArr[0] < rightArr[0]) {
-      sortedArr.push(leftArr.shift());
-    } else {
-      sortedArr.push(rightArr.shift());
+    while (leftArr.length && rightArr.length) {
+        if (leftArr[0] < rightArr[0]) {
+            sortedArr.push(leftArr.shift());
+        } else {
+            sortedArr.push(rightArr.shift());
+        }
     }
-  }
   
-  return sortedArr.concat(leftArr, rightArr);
+    return sortedArr.concat(leftArr, rightArr);
 
 }
 
